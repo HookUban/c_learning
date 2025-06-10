23ee1a8d474a48961b94995655f473f65e95eab8
HookUban <2122484643@qq.com>
2025-06-11 07:25:56 +0800

    deep_learning

--- test_debugger_release/main.c
+++ test_debugger_release/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 
 //int main(void) {
 //
@@ -24,27 +24,89 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
+//
+//char* my_strcpy(char* dest, const char* src)
+//{
+//    char* ret = dest;
+//    assert(src != NULL);
+//    assert(dest != NULL);
+//
+//    while (*dest++ = *src++)
+//    {};
+//    return ret;
+//}
 
-char* my_strcpy(char* dest, const char* src)
+//int main()
+//{
+//    char arr1[20] = "xxxxxxxxxxx";
+//    char arr2[] = "hello bit";
+//
+//    my_strcpy(arr1, arr2);
+//
+//    printf("%s\n", my_strcpy(arr1, arr2));
+//
+//    return 0;
+//}
+
+//
+//int my_strlen(const char* str)
+//{
+//    assert(str != NULL);
+//    int i = 0;
+//    while (str[i] != '\0')
+//    {
+//        i++;
+//    }
+//    return i;
+//}
+//
+//int main()
+//{
+//   char arr[] = "hello bit";
+//
+//   int i = 0;
+////   for (i = 0; i < strlen(arr); i++)
+////   {
+////       printf("%c\n", arr[i]);
+////   }
+//    for (i = 0; i < strlen(arr); i++)
+//    {
+//        printf("%c\n", *(arr+i));
+//    }
+//}
+
+char* reverse_str(char* str, int len)
 {
-    char* ret = dest;
-    assert(src != NULL);
-    assert(dest != NULL);
+    assert(str != NULL);
+    int l = 0;
+    int r = len - 1;
 
-    while (*dest++ = *src++)
-    {};
-    return ret;
+    while(l < r)
+    {
+        char temp = *(str+l);
+        *(str+l++) = *(str+r);
+        *(str+r--) = temp;
+    }
+    return str;
 }
+
+
+typedef struct
+{
+    int val;
+    struct ListNode* next;
+}ListNode;
+
 
 int main()
 {
-    char arr1[20] = "xxxxxxxxxxx";
-    char arr2[] = "hello bit";
+    char arr[10001] = {0};
+    gets(arr);
+    int len = strlen(arr);
+    char* ret = reverse_str(arr, len);
 
-    my_strcpy(arr1, arr2);
-
-    printf("%s\n", my_strcpy(arr1, arr2));
-
+    printf("%s\n", arr);
     return 0;
 }
