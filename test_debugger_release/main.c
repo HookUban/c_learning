5bff82a77faeaeda191f22011fa86dc2ae654e59
HookUban <2122484643@qq.com>
2025-06-10 14:55:34 +0800

    06_10_14_55

--- test_debugger_release/main.c
+++ test_debugger_release/main.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
 
-int main(void) {
+//int main(void) {
+//
+//    int i = 0;
+//    for (i = 0; i < 10; i++)
+//    {
+//        printf("%d\n", i);
+//    }
+//    return 0;
+//}
 
-    int i = 0;
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\n", i);
-    }
+//
+//int main()
+//{
+//    int i = 0;
+//    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//    for (i  = 0; i < 13; i++)
+//    {
+//        arr[i] = 0;
+//        printf("%d\n", arr[i]);
+//    }
+//}
+
+#include <stdio.h>
+#include <assert.h>
+
+
+char* my_strcpy(char* dest, const char* src)
+{
+    char* ret = dest;
+    assert(src != NULL);
+    assert(dest != NULL);
+
+    while (*dest++ = *src++)
+    {};
+    return ret;
+}
+
+int main()
+{
+    char arr1[20] = "xxxxxxxxxxx";
+    char arr2[] = "hello bit";
+
+    my_strcpy(arr1, arr2);
+
+    printf("%s\n", my_strcpy(arr1, arr2));
+
     return 0;
 }
