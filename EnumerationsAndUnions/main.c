b60ade74f9648000f40ca2978c6dcd7a0b9e27af
HookUban <2122484643@qq.com>
2025-06-19 14:45:11 +0800

    完成联合体和枚举

--- EnumerationsAndUnions/main.c
+++ EnumerationsAndUnions/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+
+enum Day
+{
+    Mon=1,
+    Tues,
+    Wed,
+    Thurs,
+    Fri,
+    Sat,
+    Sun
+};
+
+
+
+//int main(void) {
+//
+//    enum Day d = Fri;
+//    printf("%d\n", Mon);
+//    printf("%d\n", d);
+//
+//    return 0;
+//}
+//
+
+// 联合（共用体）
+
+union Un
+{
+    int a;
+    char c;
+};
+
+int main()
+{
+//    union Un u;
+//    u.a = 0x12345678;
+//    u.c = 0x10;
+//    printf("%d\n", sizeof(u));
+//
+//    printf("%p\n", &u);
+//    printf("%p\n", &u.a);
+//    printf("%p\n", &u.c);
     return 0;
 }
